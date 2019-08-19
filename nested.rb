ad2fcb1b6b76e0cb459deade5f25c0f93c9c8f0c
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-19 22:37:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,11 +83,9 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
+    programmer_hash[:yukihiro_matsumoto][:known_for]= "Ruby"
      programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
-  # :known_for => "Ruby",
-  # :languages => ["LISP", "C"]
- }
+  
 end
 
 def changing_alan
